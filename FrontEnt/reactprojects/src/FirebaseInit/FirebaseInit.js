88d3ee49483c4f8fde31ad0a2ea6fc34ace0d4c8
Rahul Rudra <rahulrudra665@gmail.com>
2023-07-16 08:31:16 +0600

    packages commit

---
+++ FrontEnt/reactprojects/src/FirebaseInit/FirebaseInit.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDykSvCVa3Txol-Oi6fXlPukNa_HJwoM_U",
+  authDomain: "dragonnews-5affe.firebaseapp.com",
+  projectId: "dragonnews-5affe",
+  storageBucket: "dragonnews-5affe.appspot.com",
+  messagingSenderId: "1023082141470",
+  appId: "1:1023082141470:web:0e74824a918ecc2a15db0f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
